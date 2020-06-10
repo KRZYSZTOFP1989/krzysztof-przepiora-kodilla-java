429ce2595a6c36e3a45808cd714924fc9453e672
KRZYSZTOFP1989 <kprzepiora89@o2.pl>
2020-06-10 17:42:29 +0200

    testing

--- src/main/java/com/kodilla/KodilaCourseApplication.java
+++ src/main/java/com/kodilla/KodilaCourseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodilaCourseApplication {
+public class ssKodilaCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodilaCourseApplication.class, args);
