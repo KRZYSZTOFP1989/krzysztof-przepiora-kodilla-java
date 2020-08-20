7ca2ec57a2c1cb446d235aba86728acaebc4009b
KRZYSZTOFP1989 <kprzepiora89@o2.pl>
2020-08-20 19:09:18 +0200

    zadanie 15.3

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.patterns.strategy.social;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+
+    @Test
+    public void testDefaultSharingStrategies() {
+
+        //Given
+        User giuseppe = new Millenials("Giuseppe Reyna");
+        User francesco = new YGeneration("Francesco Totti");
+        User michael = new ZGeneration("Michael Ballack");
+
+        //When
+        String giuseppeSharePost = giuseppe.sharePost();
+        System.out.println("Giuseppe share post on: " + giuseppeSharePost);
+        String francescoSharePost = francesco.sharePost();
+        System.out.println("Francesco share post on: " + francescoSharePost);
+        String michaelSharePost = michael.sharePost();
+        System.out.println("Michael share post on: " + michaelSharePost);
+
+        //Then
+        Assert.assertEquals("Facebook", giuseppeSharePost);
+        Assert.assertEquals("Twitter", francescoSharePost);
+        Assert.assertEquals("Snapchat", michaelSharePost);
+    }
+
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        User giuseppe = new Millenials("Giuseppe Reyna");
+
+        //When
+        String giuseppeSharePost = giuseppe.sharePost();
+        System.out.println("Giuseppe share post on " + giuseppeSharePost);
+        giuseppe.medium(new TwitterPublisher());
+        giuseppeSharePost = giuseppe.sharePost();
+        System.out.println("Giuseppe share post on " + giuseppeSharePost);
+
+        //Then
+        Assert.assertEquals("Twitter", giuseppeSharePost);
+    }
+}
