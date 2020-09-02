d1c29b5299f2730ffa6490aa64d24f70cc6785ed
KRZYSZTOFP1989 <kprzepiora89@o2.pl>
2020-09-02 14:32:25 +0200

    aktualizacja

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -1,16 +1,16 @@
 package com.kodilla.hibernate.tasklist.dao;
 
-import com.kodilla.hibernate.task.dao.Respository;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
-@Respository
+@Repository
+@Transactional
 public interface TaskListDao extends CrudRepository<TaskList, Integer> {
 
-    @Transactional
     List<TaskList> findByListName(String listName);
 
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kodilla.hibernate.task.dao;
+package com.kodilla.hibernate;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
