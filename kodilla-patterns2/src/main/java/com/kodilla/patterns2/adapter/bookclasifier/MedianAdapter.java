193ed6f26ea3c26c4f3a05317fa6b42023101795
KRZYSZTOFP1989 <62620232+KRZYSZTOFP1989@users.noreply.github.com>
2020-12-03 14:13:09 +0100

    Commit 30.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 
 import java.util.HashMap;
@@ -11,9 +12,9 @@ public class MedianAdapter extends MedianAdaptee implements Classifier {
 
     @Override
     public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
-        Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> bookMap = new HashMap<>();
+        Map<BookSignature, Book> bookMap = new HashMap<>();
         for(com.kodilla.patterns2.adapter.bookclasifier.librarya.Book book: bookSet) {
-            com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book book1 = new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(book.getAuthor(), book.getTitle(), book.getPublicationYear());
+            Book book1 = new Book(book.getAuthor(), book.getTitle(), book.getPublicationYear());
             bookMap.put(new BookSignature(book.getSignature()), book1);
         }
 
